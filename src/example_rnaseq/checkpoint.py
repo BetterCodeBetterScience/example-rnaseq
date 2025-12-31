779dc96b305a59e197c46cbde507bb5d0f618022
Russell Poldrack <poldrack@gmail.com>
2025-12-30 16:17:55 -0800

    fix print statement

--- src/example_rnaseq/checkpoint.py
+++ src/example_rnaseq/checkpoint.py
@@ -231,7 +231,7 @@ def run_with_checkpoint(
     from_cache = False
     error_message = None
     save_succeeded = False
-    print(*args, **kwargs)
+
     try:
         if checkpoint_file.exists() and not force:
             print(f"[{step_name}] Loading from checkpoint: {checkpoint_file.name}")
